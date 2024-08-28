06bf6de0468e5a4ea924de757db10542eaf3d06c
suneel <sknigamgkp@gmail.com>
2024-08-28 20:57:40 +0530

    i love my india

---
+++ provider copy.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.116.0"
+    }
+  }
+}
+provider "azurerm" {
+
+  subscription_id = "2892d634-8067-481b-853f-fc806a273103"
+
+  features {
+
+  }
+
+}
